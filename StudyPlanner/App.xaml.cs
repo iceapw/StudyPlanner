f7a47525f4a8adeef8df85218bde3f7b07ca8052
iceapw <apiwatianachai@gmail.com>
2026-03-18 14:39:11 -0400

    Add project files.

---
+++ StudyPlanner/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace StudyPlanner
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+		}
+
+		protected override Window CreateWindow(IActivationState? activationState)
+		{
+			return new Window(new AppShell());
+		}
+	}
+}
---
+++ StudyPlanner/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace StudyPlanner
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
