da1799c083af805d8a5eab93fd54dcd2a8625440
Jason Welsh <132491235+urboi11@users.noreply.github.com>
2026-04-07 13:36:33 -0400

    Adding various updates to Pull Request
    Addressed the Dashboard updates that Apiwat had mentioned in chat. Also removing the profile pages as that is no longer needed.

--- StudyPlanner/AppShell.xaml.cs
+++ StudyPlanner/AppShell.xaml.cs
@@ -7,4 +7,25 @@ public partial class AppShell : Shell
 		InitializeComponent();
 		Routing.RegisterRoute("CourseDetailPage", typeof(Views.Courses.CourseDetailPage));
 	}
+
+
+
+
+	protected override void OnNavigating(ShellNavigatingEventArgs args)
+	{
+    	base.OnNavigating(args);
+
+    	// Resets the stack when the user switches to a different tab
+    	if (args.Source == ShellNavigationSource.ShellSectionChanged)
+    	{
+        	var navigation = Shell.Current.Navigation;
+        	var pages = navigation.NavigationStack;
+
+        	// Remove all pages except the root (index 0)
+        	for (int i = pages.Count - 1; i >= 1; i--)
+        	{
+            	navigation.RemovePage(pages[i]);
+        	}
+    	}
+	}
 }
